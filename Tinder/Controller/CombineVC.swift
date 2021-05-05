d261d9f9fd6b11276b980e4d5960807e080a57f1
Gabriel Nicol <moraesnicol@gmail.com>
2021-05-04 23:04:44 -0300

    CombineVC init

---
+++ Tinder/Controller/CombineVC.swift
@@ -0,0 +1,16 @@
+//
+//  CombineVC.swift
+//  Tinder
+//
+//  Created by Gabriel on 04/05/21.
+//
+
+import UIKit
+
+class CombineVc: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
